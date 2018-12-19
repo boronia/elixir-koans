cbc106666663072d4c186867c1f82f0f1f17c494
Veronica Luke <veronica@expert360.com>
2018-12-19 11:49:03 +1100

    Modify code example

--- lib/koans/22_with.ex
+++ lib/koans/22_with.ex
@@ -1,43 +1,45 @@
 defmodule User do
   defstruct name: nil, age: nil
+end
 
-  def create(params) do
-    age = params[:age]
-    name = params[:name]
-
-    cond do
-      is_nil(age) or not is_integer(age) ->
-        {:error, "age must be an integer"}
-
-      is_nil(name) ->
-        {:error, "name is required"}
-
-      true ->
-        {:ok, %User{age: age, name: name}}
-    end
+defmodule Context do
+  @records %{
+    1 => %User{name: "Luke", age: 18},
+    2 => %User{age: 12, name: "Sally"}
+  }
+  def find(id) when is_integer(id) do
+    Map.get(@records, id)
   end
+
+  def find(_), do: nil
 end
 
 defmodule With do
   use Koans
 
+  def get_user(id) do
+    with user <- Context.find(id) do
+      if is_nil(user) do
+        {:error, "Failed to find user by id #{id}"}
+      else
+        {:ok, user}
+      end
+    end
+  end
+
   @intro """
   How do I refactor my conditional that does error handling using 'with'?
   """
 
-  koan "Age and name valid" do
-    assert {:ok, %User{age: 12, name: "Sally"}} == User.create(%{age: 12, name: "Sally"})
+  koan "Find user 1" do
+    assert {:ok, %User{age: 18, name: "Luke"}} == get_user(1)
   end
 
-  koan "Age invalid" do
-    assert {:error, "age must be an integer"} == User.create(%{age: "aaa", name: "Sally"})
+  koan "Find user 2" do
+    assert {:ok, %User{age: 12, name: "Sally"}} == get_user(2)
   end
 
-  koan "Name invalid" do
-    assert {:error, "name is required"} == User.create(%{age: 12, name: nil})
-  end
-
-  koan "Both invalid" do
-    assert {:error, "name and age are required"} == User.create(%{})
+  koan "Non exist user" do
+    assert {:error, "Failed to find user by id 3"} == get_user(3)
   end
 end
